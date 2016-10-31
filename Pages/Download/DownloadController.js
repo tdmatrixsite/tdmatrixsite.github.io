d8bc75f4d5525314ac7750bf25ed38f97f38cdaf
tdmatrixsite <tdmatrixsite@gmail.com>
2016-10-31 08:36:38 +0300

    Update DownloadController.js

--- Pages/Download/DownloadController.js
+++ Pages/Download/DownloadController.js
@@ -1,5 +1,5 @@
 var downloadController=function($scope)
 {
-  $scope.githubLink="";
-  $scope.downloadLink="";
+  $scope.githubLink="https://github.com/alaahamed1990/TD-MATRIX";
+  $scope.downloadLink="https://github.com/alaahamed1990/TD-MATRIX/tree/master/dist";
 }
